3cd4b0b75efc4e0cfd567dabc0c6dcf2e166b25b
myoaung99 <myomyintaung064@gmail.com>
2023-06-10 08:34:07 +0630

    doing great

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { ReportsModule } from './reports/reports.module';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { ReportsController } from './reports/reports.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
+import { Report } from './reports/reports.entity';
 
 @Module({
-  imports: [ReportsModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+    ReportsModule,
+    UsersModule,
+  ],
   controllers: [AppController, UsersController, ReportsController],
   providers: [AppService],
 })
